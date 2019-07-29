b45ea8b0e30623c087163d6ee33043693e1fe051
System Administrator <root@D-10-18-229-51.dhcp4.washington.edu>
2019-07-29 12:44:16 -0700

    added a modal

--- src/components/portfolio.js
+++ src/components/portfolio.js
@@ -4,24 +4,27 @@ import Modal from 'react-awesome-modal';
 
 export default class Porfolio extends Component {
 
-  state={
-    modal:{}
+  state = {
+    modal: {}
   }
 
-  render() {
-    const {modal} = this.state
-    return (<section id="portfolio">
-      <div className="row">
-        <div className="twelve columns collapsed">
-          <h1>Check Out Some of My Work.</h1>
-          <div id="portfolio-wrapper" className="bgrid-thirds s-bgrid-thirds cf">
+  closeModal = () => {
+    this.setState({modal:{}})
+}
+render() {
+  const { modal } = this.state
+  return (<section id="portfolio">
+    <div className="row">
+      <div className="twelve columns collapsed">
+        <h1>Check Out Some of My Work.</h1>
+        <div id="portfolio-wrapper" className="bgrid-thirds s-bgrid-thirds cf">
           {
-            resumeData.portfolio && resumeData.portfolio.map((item, i)=>{
-              return(
+            resumeData.portfolio && resumeData.portfolio.map((item, i) => {
+              return (
                 <div key={i} className="columns portfolio-item">
                   <div className="item-wrap">
-                    <div onClick={()=>this.setState({modal:item})} style={{cursor:'pointer'}}>
-                      <img src={`${item.imgurl}`} className="item-img" alt="img"/>
+                    <div onClick={() => this.setState({ modal: item })} style={{ cursor: 'pointer' }}>
+                      <img src={`${item.imgurl}`} className="item-img" alt="img" />
                       <div className="overlay">
                         <div className="portfolio-item-meta">
                           <h5>{item.name}</h5>
@@ -34,10 +37,28 @@ export default class Porfolio extends Component {
               )
             })
           }
-          </div>
         </div>
       </div>
+    </div>
+
+    <Modal
+      visible={modal&&modal.name}
+      width="600"
+      height="400"
+      effect="fadeInUp"
+      onClickAway={() => this.closeModal()}
+    >
+      <div className="portfolio-modal">
+        <div>
+        <h1>{modal.title}</h1>
+        <p>{modal.fullDescription}</p>
+        <a href={modal.githubLink} target ="b_lank" rel= "noopener noferrer"> Github Repository </a>
+        </div>
+        <button onClick={() => this.closeModal()}>Close</button>
+      </div>
+      
+    </Modal>
 
   </section>);
-  }
+}
 }
